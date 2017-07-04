a922a7d547aea35b177bf5ac12d4fb71fb89e637
Alicia Wang <aliwang2001@gmail.com>
2017-07-03 23:35:39 -0700

    added new

---
+++ js/head.js
@@ -0,0 +1,30 @@
+  document.write('\
+  <meta charset="utf-8">\
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">\
+  <meta name="viewport" content="width=device-width, initial-scale=1">\
+  <meta name="keywords" content="materials and process simulation center, MSC, caltech">\
+  <meta name="description" content="Collaborative webpage where recent research in chemistry is shared, materials and process simulation center details">\
+  <meta name="author" content="MSC, Caltech">\
+\
+  <!-- Site title\
+   ================================================== -->\
+  <title>MSC - Materials and Process Simulation Center</title>\
+  <!-- Bootstrap CSS\
+   ================================================== -->\
+  <link rel="stylesheet" href="css/bootstrap.min.css">\
+  <!-- Animate CSS\
+   ================================================== -->\
+  <link rel="stylesheet" href="css/animate.min.css">\
+  <!-- Font Icons CSS\
+   ================================================== -->\
+  <link rel="stylesheet" href="css/font-awesome.min.css">\
+  <link rel="stylesheet" href="css/ionicons.min.css">\
+  <!-- Main CSS\
+   ================================================== -->\
+  <link rel="stylesheet" href="css/style.css">\
+\
+  <!-- Google web font \
+   ================================================== --> \
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300" rel="stylesheet" type="text/css">\
+\
+');
