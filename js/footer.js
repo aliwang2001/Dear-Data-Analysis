41213ca3160ed9f139f62648db12792b01665719
Alicia Wang <aliwang2001@gmail.com>
2017-07-06 14:57:53 -0700

    new update

--- js/footer.js
+++ js/footer.js
@@ -5,7 +5,7 @@ document.write('\
   <div class="container">\
     <div class="row">\
       <div class="col-md-3 col-sm-3 vert-line">\
-      <a href="http://www.caltech.edu/"><img src="images/caltech-logo.png" class="img-responsive resize" alt="Blog"></a>\
+      <a href="http://www.caltech.edu/"><img src="../images/caltech-logo.png" class="img-responsive resize" alt="Blog"></a>\
       </div>\
       <div class="col-md-7 col-sm-7">\
         <p class="wow fadeInUp"  data-wow-delay="0.2s">Copyright© 2016 MSC@Caltech</p>\
